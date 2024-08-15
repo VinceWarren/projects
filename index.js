5393b009e232db894ddd30181edefab9ec79ac9d
Vince Warren <83796494+VinceWarren@users.noreply.github.com>
2024-08-15 12:22:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const serverless = require('serverless-http');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const BLOG = require('./models/blog'); // Adjust the path as needed
