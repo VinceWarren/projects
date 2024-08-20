b38eabde6a616e44ee3a4a2877dc9e576a3c4d56
Vince Warren <83796494+VinceWarren@users.noreply.github.com>
2024-08-20 17:14:19 +0800

    ngewk

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const BLOG = require('./models/blog'); // Adjust the path as needed
+const BLOG = require('./models/blog');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
